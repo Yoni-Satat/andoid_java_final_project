5d6cd67275a8896b55b89effc1f25da17565a48d
Yoni Satat <yoni.satat@gmail.com>
2017-11-18 15:43:01 +0000

    created Task class and TaskTest, all tests passed

---
+++ app/src/main/java/codeclan/com/todoapp/models/Task.java
@@ -0,0 +1,8 @@
+package codeclan.com.todoapp.models;
+
+/**
+ * Created by user on 18/11/2017.
+ */
+
+public class Task {
+}
---
+++ app/src/test/java/codeclan/com/todoapp/TaskTest.java
@@ -0,0 +1,8 @@
+package codeclan.com.todoapp;
+
+/**
+ * Created by user on 18/11/2017.
+ */
+
+public class TaskTest {
+}
